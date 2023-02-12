462f6cf570027505a6f142a676ab462698507ab1
ArtemParhom <artkievukr@ukr.net>
2023-02-12 18:45:40 +0200

    add js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this line 
 
-console.log(galleryItems);
+const imageContainerEl = document.querySelector(`.gallery`);
+
+imageContainerEl.addEventListener(`click`, onImageContainerClick);
+
+const imageMarkup = createImageCardMarkup(galleryItems);
+
+imageContainerEl.insertAdjacentHTML(`beforeend`, imageMarkup);
+
+function createImageCardMarkup(galleryImage) {
+    return galleryImage.map(({ preview,
+        original,
+        description }) => {
+        return `
+        <div class="gallery__item">
+  <a class="gallery__link" href=${original} >
+         <img class="gallery__image"
+     src=${preview}
+     data-source=${original}
+     alt=${description}/>
+     </a>
+</div>`;
+    }).join(``);  
+}; 
+
+function onImageContainerClick(evt) {
+    
+    if (evt.target !== "IMAGE") { 
+        const dataSrcImage = evt.target.dataset.source;
+        const instance = basicLightbox.create(`
+    <img src="${dataSrcImage}" width="1280" height="600" >
+`); 
+    return instance.show(); 
+    };
+    return;
+};
+
+
+
+
+
+ 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(`.gallery`);
+
+const imageMarkup = createImageCardMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML(`beforeend`, imageMarkup);
+
+const galleryImageEl = document.querySelector(`.gallery__image`);
+
+galleryImageEl.addEventListener(`click`, onClickOfImage);
+
+let gallery = new SimpleLightbox('.gallery a');
+
+function onClickOfImage(){
+    gallery.on('show.simplelightbox', function () {
+       
+}); 
+};
+
+//---------------------------------------
+function createImageCardMarkup(galleryImage) {
+    return galleryImage.map(({ preview,
+        original,
+        description }) => {
+        return `<li>   
+  <a class="gallery__item" href=${original}><img class="gallery__image" src=${preview} alt=${description}/>
+     </a></li>
+`;
+    }).join(``);  
+};
